c7b805987f8e821221e260d5ac36a595db975d38
Rordrie <152210227+Rordrie@users.noreply.github.com>
2024-01-06 17:13:16 +0100

    Add files via upload

---
+++ sport.js
@@ -0,0 +1,35 @@
+
+	const bar = document.querySelector('.bar')
+	const menu = document.querySelector('.lines').addEventListener('click',
+	function(e) {
+		if (bar.classList.contains('hide')){
+			bar.classList.remove('hide')
+
+	}else(bar.classList.add('hide'))
+		})
+
+	const boy = document.querySelector('.crack')
+	const space = document.querySelector('.all')
+	const girl = document.querySelector('.box').addEventListener('click',
+	function(e) {
+		if (boy.classList.contains('hide')){
+			boy.classList.remove('hide')
+
+
+
+	}else(boy.classList.add('hide'))
+		
+		})
+
+	const lady = document.querySelector('.bar')
+	const man = document.querySelector('.part3').addEventListener('click',
+		function(e) {
+			 if (lady.classList.contains('hide')){
+				lady.classList.remove('hide')
+
+
+
+	}else(lady.classList.add('hide'))
+		
+		})
+
